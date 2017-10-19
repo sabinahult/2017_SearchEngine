ec7805db676ba0e7753a02e796fa46cff8ea6e45
Lisa Hartmann Jensen <lisj@itu.dk>
2017-10-19 13:51:14 +0200

    Included test-classes (that are empty)

--- searchengine/test/FileHelperTest.java
+++ searchengine/test/FileHelperTest.java
@@ -17,12 +17,12 @@ class FileHelperTest {
 
     @Test
     void parseBadFile() {
-//        List<Website> sites = FileHelper.parseFile("test-resources/test-file-with-errors.txt");
-//        assertEquals(2, sites.size());
-//        assertEquals("title1", sites.get(0).getTitle());
-//        assertEquals("title2", sites.get(1).getTitle());
-//        assertTrue(sites.get(0).containsWord("word1"));
-//        assertFalse(sites.get(0).containsWord("word3"));
+        List<Website> sites = FileHelper.parseFile("test-resources/test-file-with-errors.txt");
+        assertEquals(2, sites.size());
+        assertEquals("title1", sites.get(0).getTitle());
+        assertEquals("title2", sites.get(1).getTitle());
+        assertTrue(sites.get(0).containsWord("word1"));
+        assertFalse(sites.get(0).containsWord("word3"));
 
     }
 }
---
+++ searchengine/test/InvertedIndexTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InvertedIndexTest {
+
+    @Test
+    void build() {
+    }
+
+    @Test
+    void lookup() {
+    }
+
+}
