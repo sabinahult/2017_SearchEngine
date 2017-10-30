98d901ef9379ba0bc7461bb4369789740bd93bb9
Lisa Hartmann Jensen <lisj@itu.dk>
2017-10-30 19:31:43 +0100

    Included tests for the classes Query and Website.

--- searchengine/test/QueryTest.java
+++ searchengine/test/QueryTest.java
@@ -2,30 +2,35 @@ import database.Website;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import performance.Query;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class QueryTest {
     @BeforeEach
     void setUp() {
         Query queryObject = new Query();
 
-        String query1 = "Queen Denmark";
-        String query2 = "Queen Denmark OR President USA";
-        String query3 = "Queen Denmark OR President USA OR Chancellor Germany";
+        String query1 = " "; //empty search string
+        String query2 = "handsome"; //word that is not there
+        String query3 = "adam";
+        String query4 = "queen denmark";
+        String query5 = "president jens OR snake";
+        String query6 = "denmark queen OR president usa OR chancellor germany";
+        String query7 = "adam eve snake apple bible";//contains five consecutive words
+        String query8 = "";//empty
+        String query9 = "president jimmy carter OR  adam eve bible";//contains three words and three words
+        String query10 = "quen denmark";//contains two words - one of them spelled wrong to make an empty list
 
         List<Website> sites = new ArrayList<>();
         sites.add(new Website("example1.com", "Example 1", Arrays.asList("queen")));
-        sites.add(new Website("example2.com", "Example 1", Arrays.asList("queen", "denmark")));
-        sites.add(new Website("example3.com", "Example 1", Arrays.asList("president")));
-        sites.add(new Website("example4.com", "Example 1", Arrays.asList("president", "usa")));
-        sites.add(new Website("example5.com", "Example 2", Arrays.asList("queen", "president", "jens")));
-        sites.add(new Website("example6.com", "Example 3", Arrays.asList("denmark", "president", "chancellor", "germany", "queen", "USA")));
-        sites.add(new Website("example7.com", "Example 4", Arrays.asList("adam", "eve", "snake", "apple")));
+        sites.add(new Website("example2.com", "Example 2", Arrays.asList("queen", "denmark")));
+        sites.add(new Website("example3.com", "Example 3", Arrays.asList("president", "jimmy", "carter")));
+        sites.add(new Website("example4.com", "Example 4", Arrays.asList("queen", "president", "jens")));
+        sites.add(new Website("example5.com", "Example 5", Arrays.asList("denmark", "president", "chancellor", "germany", "queen", "USA")));
+        sites.add(new Website("example6.com", "Example 6", Arrays.asList("adam", "eve", "snake", "apple")));
 
     }
 
@@ -35,10 +40,18 @@ class QueryTest {
 
     @Test
     void test(){
-        //Given two words the method needs to return a List of websites that contain both words
-        //assertEquals(2, queryObject.makeQuery(query1).size());
-        //assertEquals(2, queryObject.makeQuery(query2).size());
-        //assertEquals(2, queryObject.makeQuery(query3).size());
+        //assertEquals(0, queryObject.makeQuery(query1).size());
+        //assertEquals(0, queryObject.makeQuery(query2).size());
+        //assertEquals(1, queryObject.makeQuery(query3).size());
+        //assertEquals(2, queryObject.makeQuery(query4).size());
+        //assertEquals(2, queryObject.makeQuery(query5).size());
+        //assertEquals(1, queryObject.makeQuery(query6).size());
+        //assertEquals(0, queryObject.makeQuery(query7).size());
+        //assertEquals(0, queryObject.makeQuery(query8).size());
+        //assertEquals(1, queryObject.makeQuery(query9).size());
+        //assertEquals(0, queryObject.makeQuery(query10).size());
+        //assertTrue("example4.com", queryObject.makeQuery(query4).getUrl();
+        //assertFalse("example3.com", queryObject.makeQuery(query5).getUrl();
     }
 
 
---
+++ searchengine/test/WebsiteTest.java
@@ -0,0 +1,49 @@
+
+import database.Website;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WebsiteTest {
+    private static Website site1;
+    private static Website site2;
+    private static Website site3;
+    private static Website site4;
+
+    @BeforeAll
+    static void setUp(){
+        site1 = new Website("https://anothertitle.com", "Another title", Arrays.asList("adam", "zebra", "snake"));
+        site2 = new Website("", "Empty URL", Arrays.asList("one", "word"));
+        site3 = new Website("https://al-bank.dk", "", Arrays.asList("banking", "denmark", "bank", "currency"));
+        site4 = new Website("https://callme.dk", "Phonecompany", Arrays.asList());
+    }
+
+    @Test
+    void getTitle() {
+        assertEquals("Another title", site1.getTitle());
+        assertEquals("", site3.getTitle());
+    }
+
+    @Test
+    void getUrl() {
+        assertEquals("https://al-bank.dk", site3.getUrl());
+        assertEquals("", site2.getUrl());
+    }
+
+    @Test
+    void getWords() {
+        assertEquals(4, site3.getWords().size());
+        assertEquals(0, site4.getWords().size());
+    }
+
+    @Test
+    void containsWord() {
+        assertTrue(site3.getWords().get(0).contains("banking"));
+        assertFalse(site3.getWords().get(3).contains("denmark"));
+    }
+
+
+}
