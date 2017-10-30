eab6cb27fae240d9c7ddf3ef026235fdf1f5488a
sahu <sahu@itu.dk>
2017-10-30 15:58:29 +0100

    Query Class so far with calling it in SearchEngine. Small corrections to TinyTimer Class.

--- searchengine/src/Query.java
+++ searchengine/src/Query.java
@@ -1,5 +1,56 @@
+import database.Website;
+import index.Index;
+import java.util.*;
+
 public class Query {
+    private List<Website> foundSites = new ArrayList<>();
+    private List<Website> finalResult = new ArrayList<>();
 
-    //Remember to covert query to lower case
+    /**
+     * Takes a string of words, and splits them around or. For each of the returned strings it splits them further around a
+     * white space into an array of single words. Then for each of those single words it calls the lookup method of the
+     * index, which returns a list of websites that contain the word. If the foundSites variable is empty, it adds all
+     * elements from the lookup into the list of foundSites. If foundSites already has elements, it keeps only those
+     * elements that are the same between the two collections, because we only want to return websites that contains
+     * all words in the given part of the query. When all words in the given string has been looked up, it finally adds
+     * all the found websites to finalResult. And then it does it all again for the next part of the query, if there
+     * is more. This method can take any number of substrings of a query and any number of words in a substring.
+     * @param query The input from the user passed from SearchEngine
+     * @param index The database to search in for the given input passed from SearchEngine
+     * @return A list of website objects that contain all of the given words in the query
+     */
+    public List<Website> runQuery(String query, Index index) {
+        String queryLowerCase = query.toLowerCase();
 
+        String[] queryLines = queryLowerCase.split(" or ");
+
+        for (String line : queryLines) {
+            String[] words = line.split(" ");
+
+            boolean isFound = true;
+
+            for (String word : words) {
+                List<Website> tempList = index.lookup(word);
+
+                if(tempList.isEmpty()) {
+                    isFound = false;
+                    foundSites.clear();
+                }
+
+                while(isFound) {
+
+                    if(foundSites.isEmpty()) {
+                        foundSites.addAll(tempList);
+                    } else {
+                        foundSites.retainAll(tempList);
+                    }
+                }
+            }
+
+            finalResult.addAll(foundSites);
+            foundSites.clear();
+        }
+
+        return finalResult;
+    }
 }
--- searchengine/src/SearchEngine.java
+++ searchengine/src/SearchEngine.java
@@ -17,7 +17,7 @@ import java.util.Scanner;
 public class SearchEngine {
     public static void main(String[] args) {
         System.out.println("Welcome to the SearchEngine!");
-        if (args.length != 1) {
+        if(args.length != 1) {
             System.out.println("Error: Filename is missing");
             return;
         }
@@ -29,7 +29,7 @@ public class SearchEngine {
         //Debugging to see contents of websites array after it's created from reading the file
         //System.out.println(sites.toString());
 
-        //Made a constructor in the index.InvertedIndex class, that takes a map, so when declaring it here
+        //Made a constructor in the InvertedIndex class, that takes a map, so when declaring it here
         //we are deciding which implementation to use, instead of changing it in the class itself every time
         Index index = new InvertedIndex(new HashMap<>());
         index.build(sites);
@@ -37,12 +37,19 @@ public class SearchEngine {
         System.out.println("Please provide a query word");
 
         //SEARCH LOOP
-        while (sc.hasNext()) {
+        while(sc.hasNext()) {
             TinyTimer timer = new TinyTimer();
             timer.start();
 
+            //Added .toLowerCase()
             String line = sc.nextLine();
-            foundSites = index.lookup(line);
+
+            //This is no longer needed when using the Query class
+            //foundSites = index.lookup(line);
+
+            //Using the new Query class
+            Query query = new Query();
+            foundSites = query.runQuery(line, index);
 
             if(foundSites.isEmpty()) {
                 System.out.println("No website contains " + line);
