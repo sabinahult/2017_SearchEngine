162a9ed2c2f1013d1c7a0b05c097165ec90ebcfd
Lisa Hartmann Jensen <lisj@itu.dk>
2017-10-31 13:11:49 +0100

    Added test for Query-class

--- searchengine/test/QueryTest.java
+++ searchengine/test/QueryTest.java
@@ -1,29 +1,22 @@
 import database.Website;
-import org.junit.jupiter.api.AfterEach;
+import index.Index;
+import index.InvertedIndex;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import performance.Query;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class QueryTest {
+    private Query queryObject;
+    private Index index;
+
     @BeforeEach
     void setUp() {
-        Query queryObject = new Query();
-
-        String query1 = " "; //empty search string
-        String query2 = "handsome"; //word that is not there
-        String query3 = "adam";
-        String query4 = "queen denmark";
-        String query5 = "president jens OR snake";
-        String query6 = "denmark queen OR president usa OR chancellor germany";
-        String query7 = "adam eve snake apple bible";//contains five consecutive words
-        String query8 = "";//empty
-        String query9 = "president jimmy carter OR  adam eve bible";//contains three words and three words
-        String query10 = "quen denmark";//contains two words - one of them spelled wrong to make an empty list
-
         List<Website> sites = new ArrayList<>();
         sites.add(new Website("example1.com", "Example 1", Arrays.asList("queen")));
         sites.add(new Website("example2.com", "Example 2", Arrays.asList("queen", "denmark")));
@@ -32,27 +25,37 @@ class QueryTest {
         sites.add(new Website("example5.com", "Example 5", Arrays.asList("denmark", "president", "chancellor", "germany", "queen", "USA")));
         sites.add(new Website("example6.com", "Example 6", Arrays.asList("adam", "eve", "snake", "apple")));
 
-    }
-
-    @AfterEach
-    void tearDown() {
+        queryObject = new Query();
+        index = new InvertedIndex(new HashMap<>());
+        index.build(sites);
     }
 
     @Test
-    void test(){
-        //assertEquals(0, queryObject.makeQuery(query1).size());
-        //assertEquals(0, queryObject.makeQuery(query2).size());
-        //assertEquals(1, queryObject.makeQuery(query3).size());
-        //assertEquals(2, queryObject.makeQuery(query4).size());
-        //assertEquals(2, queryObject.makeQuery(query5).size());
-        //assertEquals(1, queryObject.makeQuery(query6).size());
-        //assertEquals(0, queryObject.makeQuery(query7).size());
-        //assertEquals(0, queryObject.makeQuery(query8).size());
-        //assertEquals(1, queryObject.makeQuery(query9).size());
-        //assertEquals(0, queryObject.makeQuery(query10).size());
-        //assertTrue("example4.com", queryObject.makeQuery(query4).getUrl();
-        //assertFalse("example3.com", queryObject.makeQuery(query5).getUrl();
+    void testSingleWord(){
+        assertEquals(4, queryObject.runQuery("queen", index).size());
+        assertEquals(2, queryObject.runQuery("denmark", index).size());
+        assertEquals("Example 3", queryObject.runQuery("president", index).get(0).getTitle());
     }
 
+    @Test
+    void testMultipleWord(){
+        assertEquals(1, queryObject.runQuery("president jimmy", index).size());
+        assertEquals(2, queryObject.runQuery("queen denmark", index).size());
+        assertEquals(0, queryObject.runQuery("queen denmark prince", index).size());
+    }
 
+    @Test
+    void testORQueries(){
+        assertEquals(4, queryObject.runQuery("queen or queen", index).size());
+        assertEquals(4, queryObject.runQuery("president or adam", index).size());
+        assertEquals(1, queryObject.runQuery("hans or eve", index).size());
+    }
+
+    @Test
+    void testCornerCases(){
+        assertEquals(0, queryObject.runQuery("", index).size());
+        assertEquals(0, queryObject.runQuery("handsome", index).size());
+        assertEquals(0, queryObject.runQuery(" ", index).size());
+        assertEquals(0, queryObject.runQuery("høne", index).size());
+    }
 }
