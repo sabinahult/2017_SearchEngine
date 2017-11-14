9e40211a14f8552107e44d6141c6d1104332cf86
Susan <sugd@itu.dk>
2017-11-14 12:46:58 +0100

    We created a method in the class called tfScore.

--- searchengine/src/ranking/TFScore.java
+++ searchengine/src/ranking/TFScore.java
@@ -3,10 +3,23 @@ package ranking;
 import database.Website;
 import index.Index;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class TFScore implements Score {
+
 
     @Override
     public int getScore(String word, Website site, Index index) {
-        return 0;
+
+        List<String> wordsOnWebpage = site.getWords();
+        int occuranceOfWords = 0;
+
+        for (String s : wordsOnWebpage){
+            if(s.equals(word)){
+                occuranceOfWords++;
+            }
+        }
+        return occuranceOfWords;
     }
 }
