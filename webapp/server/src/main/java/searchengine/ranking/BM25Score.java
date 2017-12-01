4334a56afb7671295cb7f65e7578e917a211fa09
sahu <sahu@itu.dk>
2017-12-01 11:35:15 +0100

    Simplifying a but in BM25.

--- webapp/server/src/main/java/searchengine/ranking/BM25Score.java
+++ webapp/server/src/main/java/searchengine/ranking/BM25Score.java
@@ -6,46 +6,31 @@ import searchengine.index.Index;
 import java.util.List;
 
 public class BM25Score implements Score {
-
     private double avdl;
 
     public BM25Score(Index index){
         avdl = getAverageWordsInDatabase(index);
     }
 
-
     @Override
     public double getScore(String word, Website site, Index index) {
         TFScore tf = new TFScore();
         double tfScore = tf.getScore(word, site, index);
-        double k= 1.75;
-        double b = 0.75;
-        int dl = getWordsOnWebsite(site);
-        double bm25score = (tfScore*(k+1))/(k*(1-b+b*dl/avdl)+tfScore);
-        return bm25score;
-    }
-
-
-    /**
-     * Takes the website and looks up how many words there are on the site. (DL)
-     * @param site, a Website.
-     * @return the number of words on the website
-     */
-    public int getWordsOnWebsite(Website site){
-        int numberOfWords;
-        return numberOfWords = site.getWords().size();
+        double k= 1.75, b = 0.75;
+        int dl = site.getWords().size();
+        return (tfScore*(k+1))/(k*(1-b+b*dl/avdl)+tfScore);
     }
 
     /**
-     * Takes the index and calculates the average number of words over all websites in the index.(AVDL)
+     * Takes the index and calculates the average number of words over all websites in the index (AVDL)
      * @param index
      * @return a double, the average.
      */
-    public double getAverageWordsInDatabase(Index index){
+    private double getAverageWordsInDatabase(Index index){
         List<Website> websitesInDatabase = index.getWebsites();
         int sizeOfDatabase = index.getNumberOfWebsites();
         int counter = 0;
-        for(Website site: websitesInDatabase){
+        for(Website site : websitesInDatabase){
             counter = counter + site.getWords().size();
         }
         return counter/sizeOfDatabase;
